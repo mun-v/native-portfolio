590dddeb06469c18f09c8a4907070f226462f541
Victoria Muñoz <mun.v203@gmail.com>
2022-08-04 22:51:22 -0500

    Swipe and Delete Favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,31 +1,52 @@
-import { useSelector } from "react-redux";
-import { View, FlatList, Text } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 import Loading from "../components/LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { SwipeRow } from "react-native-swipe-list-view";
+import { toggleFavorite } from "../features/favorites/favoritesSlice";
 
 const FavoritesScreen = ({ navigation }) => {
   const { coursesArray, isLoading, errMess } = useSelector(
     (state) => state.courses
   );
   const favorites = useSelector((state) => state.favorites);
+  const dispatch = useDispatch();
 
   const renderFavoriteItem = ({ item: course }) => {
     return (
-      <ListItem
-        onPress={() =>
-          navigation.navigate("Directory", {
-            screen: "CourseInfo",
-            params: { course },
-          })
-        }
-      >
-        <Avatar rounded source={{ uri: baseUrl + course.image }} />
-        <ListItem.Content>
-          <ListItem.Title>{course.name}</ListItem.Title>
-          <ListItem.Subtitle>{course.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
+      <SwipeRow rightOpenValue={-100}>
+        <View style={styles.deleteView}>
+          <TouchableOpacity
+            style={styles.deleteTouchable}
+            onPress={() => dispatch(toggleFavorite(course.id))}
+          >
+            <Text style={styles.deleteText}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <ListItem
+            onPress={() =>
+              navigation.navigate("Directory", {
+                screen: "CourseInfo",
+                params: { course },
+              })
+            }
+          >
+            <Avatar rounded source={{ uri: baseUrl + course.image }} />
+            <ListItem.Content>
+              <ListItem.Title>{course.name}</ListItem.Title>
+              <ListItem.Subtitle>{course.description}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </View>
+      </SwipeRow>
     );
   };
 
@@ -47,5 +68,26 @@ const FavoritesScreen = ({ navigation }) => {
     />
   );
 };
+
+const styles = StyleSheet.create({
+  deleteView: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flex: 1,
+  },
+  deleteTouchable: {
+    backgroundColor: "red",
+    height: "100%",
+    justifyContent: "center",
+  },
+  deleteText: {
+    color: "white",
+    fontWeight: "700",
+    textAlign: "center",
+    fontSize: 16,
+    width: 100,
+  },
+});
 
 export default FavoritesScreen;
