acf9a549f60448ed81291b6a05a4733a8f0fdb53
Victoria Muñoz <mun.v203@gmail.com>
2022-08-05 15:44:57 -0500

    Persist Redux Store

--- redux/store.js
+++ redux/store.js
@@ -4,13 +4,38 @@ import { commentsReducer } from "../features/comments/commentsSlice";
 import { instructorsReducer } from "../features/instructors/instructorsSlice";
 import { promotionsReducer } from "../features/promotions/promotionsSlice";
 import { favoritesReducer } from "../features/favorites/favoritesSlice";
+import {
+  persistStore,
+  persistCombineReducers,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const config = {
+  key: "root",
+  storage: AsyncStorage,
+  debug: true,
+};
 
 export const store = configureStore({
-  reducer: {
+  reducer: persistCombineReducers(config, {
     courses: coursesReducer,
     comments: commentsReducer,
     instructors: instructorsReducer,
     promotions: promotionsReducer,
     favorites: favoritesReducer,
-  },
+  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
