e7b23d7aa1ca3c7e7234b3129ee0c25bfa79b6f0
Victoria Muñoz <mun.v203@gmail.com>
2022-08-09 21:30:19 -0500

    Sending Email

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,17 @@
 import { Text, ScrollView } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 const ContactScreen = () => {
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      recipients: ["events@amiradanceproductions.com"],
+      subject: "Inquiry",
+      body: "To whom it may concern:",
+    });
+  };
+
   return (
     <ScrollView>
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -14,6 +23,19 @@ const ContactScreen = () => {
           <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
           <Text>Phone: 1-316-221-0648</Text>
           <Text>Email: events@amiradanceproductions.com</Text>
+          <Button
+            title="Send Email"
+            buttonStyle={{ backgroundColor: "#F8751B", margin: 40 }}
+            icon={
+              <Icon
+                name="envelope-o"
+                type="font-awesome"
+                color="#fff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            onPress={() => sendMail()}
+          />
         </Card>
       </Animatable.View>
     </ScrollView>
